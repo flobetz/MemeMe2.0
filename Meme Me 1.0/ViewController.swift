ef3d9e0a84028791711a66a8b0c51dd28ab18af3
Florian Betz <florian.betz@daimler.com>
2021-03-08 17:06:46 +0100

    Added first version of the famous Meme Me App

--- Meme Me 1.0/ViewController.swift
+++ Meme Me 1.0/ViewController.swift
@@ -7,13 +7,219 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    let albumController = UIImagePickerController()
+    let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedString.Key.strokeWidth: NSNumber(-3.0)
+    ]
+    
+    // MARK: View methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // connect delegates with UI elements
+        albumController.delegate = self
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super .viewWillAppear(animated)
+        
+        // set default values for text labels
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = .center
+        topTextField.text = "TOP"
+        
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = .center
+        bottomTextField.text = "BOTTOM"
+        
+        // check if camera is available on the device
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        
+        // disable cancel button at the beginning
+        cancelButton.isEnabled = false
+        
+        // disable share button at the beginning
+        shareButton.isEnabled = false
+        
+        // subscribe to keyboard notifications
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
     }
 
-
+    // MARK: IBActions
+    @IBAction func albumButtonPressed(_ sender: UIBarButtonItem) {
+        albumController.sourceType = .photoLibrary
+        present(albumController, animated: true, completion: nil)
+    }
+    
+    @IBAction func photoButtonPressed(_ sender: UIBarButtonItem) {
+        albumController.sourceType = .camera
+    }
+    
+    @IBAction func shareButtonPressed(_ sender: UIBarButtonItem) {
+        let myMeme = save()
+        let shareController = UIActivityViewController(activityItems: [myMeme.memedImage], applicationActivities: nil)
+        shareController.completionWithItemsHandler = { (activity: UIActivity.ActivityType?, completed: Bool, arrayReturnedItems: [Any]?, error: Error?) in
+            if completed {
+                self.saveMemeToDocumentsDirectory(meme: myMeme)
+            }
+        }
+        present(shareController, animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        imageView.image = nil
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        shareButton.isEnabled = false
+        cancelButton.isEnabled = false
+    }
+    
+    // MARK: Delegate methods
+    
+    // MARK: image picker delegate methods
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            cancelButton.isEnabled = true
+            shareButton.isEnabled = true
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: UITextFieldDelegate methods
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == topTextField {
+            if topTextField.text == "TOP" {
+                topTextField.text = ""
+            }
+        } else {
+            if bottomTextField.text == "BOTTOM" {
+                bottomTextField.text = ""
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            if textField == topTextField {
+                textField.text = "TOP"
+                cancelButton.isEnabled = false
+                shareButton.isEnabled = false
+            } else {
+                textField.text = "BOTTOM"
+                cancelButton.isEnabled = false
+                shareButton.isEnabled = false
+            }
+        } else {
+            cancelButton.isEnabled = true
+            shareButton.isEnabled = true
+        }
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: keyboard methods
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if bottomTextField.isEditing {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    // MARK: meme object
+    struct meme {
+        let topText: String?
+        let bottomText: String?
+        let originalImage: UIImage?
+        let memedImage: UIImage
+    }
+    
+    func save() -> meme {
+        let myMeme = meme(topText: topTextField.text, bottomText: bottomTextField.text, originalImage: imageView.image, memedImage: generateMemedImage())
+        return myMeme
+    }
+    
+    func generateMemedImage() -> UIImage {
+        // hide tool and navbar
+        toolBar.isHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        // enable tool and navbar
+        toolBar.isHidden = false
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        
+        return memedImage
+    }
+    
+    func saveMemeToDocumentsDirectory(meme: meme) {
+        // get documents directory url
+        let documentDir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        // define image name
+        let fileName: String
+        if let text = meme.topText {
+            fileName = text + ".jpg"
+        } else {
+            fileName = "myMeme.jpg"
+        }
+        // define filepath where the meme gets saved
+        let filePath = documentDir.appendingPathComponent(fileName)
+        if let data = meme.memedImage.jpegData(compressionQuality: 1.0) {
+            do {
+                try data.write(to: filePath)
+            } catch {
+                print("failed to save the meme!", error)
+            }
+        }
+    }
 }
 
