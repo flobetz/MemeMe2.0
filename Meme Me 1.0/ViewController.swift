9d96cafca2613c081c3e6837f9f7d5321862061a
Florian Betz <florian.betz@daimler.com>
2021-03-08 09:33:14 +0100

    Initial Commit

---
+++ Meme Me 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meme Me 1.0
+//
+//  Created by Betz, Florian (059) on 08.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
